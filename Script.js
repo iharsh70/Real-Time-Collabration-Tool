221ad019d585569da5dbff6d01f8a3ede2dc0b10
harshimmadi04 <146112640+iharsh70@users.noreply.github.com>
2024-12-21 20:58:07 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+// Placeholder for handleLogin function
+function handleLogin() {
+    console.log("Login function to be implemented.");
+}
+
+// Placeholder for handleSubmit function
+function handleSubmit() {
+    console.log("Submit function to be implemented.");
+}
